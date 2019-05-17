2d7547d2be97ddeec5a52a5faa72f9a62e1ad3d2
mich <843298603@qq.com>
2019-05-17 19:57:28 +0800

    '

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -26,7 +26,7 @@ class CommentList extends Component{
                 index={i}
                  comment={comment}
                  />)}
-            </div>
+            </div> 
         )
     }
 }
