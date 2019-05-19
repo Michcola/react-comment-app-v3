ce4f47c12f900d694f828bbc8e6e2ae1d96c8723
mich <843298603@qq.com>
2019-05-19 22:53:01 +0800

    '

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -82,7 +82,7 @@ export default class CommentInput extends Component {
                         发布
                     </button>
                 </div>
-            </div> 
+            </div>
         )
     }
 }
